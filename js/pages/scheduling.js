3df820cf246129b184db0858d335024308141301
mike <xarcy123@gmail.com>
2025-06-26 09:16:17 -0400

    Update scheduling.js

--- js/pages/scheduling.js
+++ js/pages/scheduling.js
@@ -180,7 +180,7 @@ export function handleSchedulingPage() {
                             <span style="font-size: 0.9em;">${startTimeString} - ${endTimeString}</span><br>
                             <span style="color: #ddd;">${shift.location_name || ''}</span>
                             <button class="delete-shift-btn" data-shift-id="${shift.id}">
-                                <svg xmlns="[http://www.w3.org/2000/svg](http://www.w3.org/2000/svg)" width="12" height="12" fill="currentColor" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
                             </button>
                         `;
                         // Add a title attribute for tooltip on hover
@@ -425,4 +425,4 @@ export function handleSchedulingPage() {
     createDailyHoursInputs(); // Populate the daily hours input fields
     renderCalendar(currentStartDate); // Render the calendar for the initial week
     populateDropdowns(); // Populate the employee and location dropdowns
-
+}
