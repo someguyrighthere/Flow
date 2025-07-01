c7a6cfa7d0a8578cd5fe2514b74c2d66007b6229
mike <xarcy123@gmail.com>
2025-07-01 17:22:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ apiRoutes.get('/applicants', isAuthenticated, isAdmin, async (req, res) => {
 onboardingRoutes(apiRoutes, pool, isAuthenticated, isAdmin);
 
 // Fallback for serving index.html on any non-API route
-app.get('*', (req, res) => {
+app.get(/'*'/, (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
