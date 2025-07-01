bdfa5a4d84270adbaf53ac6cb9a3e35a4032f9aa
mike <xarcy123@gmail.com>
2025-06-30 21:57:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ app.use('/api', apiRoutes);
 
 // --- 8. Static File Serving & Fallback ---
 app.use(express.static(path.join(__dirname)));
-app.get(/'*'//, (req, res) => {
+app.get(/'*'/, (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
